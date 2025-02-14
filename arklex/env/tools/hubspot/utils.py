8849fc29e21030844760bb57553eba022492c1a1
Lingxiao-Chen-arklex <lingxiao.chen@arklex.ai>
2025-02-13 20:41:59 -0500

    move functions to the clean version of main branch

---
+++ arklex/env/tools/hubspot/utils.py
@@ -0,0 +1,1 @@
+HUBSPOT_AUTH_ERROR = "error: missing some or all required hubspot authentication parameters: access_token. Please set up 'fixed_args' in the config file. For example, {'name': <unique name of the tool>, 'fixed_args': {'token': <shopify_access_token>, 'shop_url': <shopify_shop_url>, 'api_version': <Shopify API version>}}"
