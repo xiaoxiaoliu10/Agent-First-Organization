0a69bd77912cbb0bb5d552758ebd5d37ef248690
Yunan Lu <luyunan0404@gmail.com>
2025-03-07 17:05:34 -0500

    clean nlu and slotfilling apis

--- model_api.py
+++ model_api.py
@@ -1,59 +1,21 @@
 import os
 import logging
-import string
-import subprocess
-import signal
-import atexit
 from typing import Dict
 import json
 from http import HTTPStatus
 import argparse
-
-from arklex.env.env import Env
 import uvicorn
 
 from openai import OpenAI
 from fastapi import FastAPI, Response
 
+from arklex.env.env import Env
 from arklex.orchestrator.orchestrator import AgentOrg
-from create import API_PORT
 from arklex.utils.model_config import MODEL
 
-NLUAPI_ADDR = f"http://localhost:{API_PORT}/nlu"
-SLOTFILLAPI_ADDR = f"http://localhost:{API_PORT}/slotfill"
 
 logger = logging.getLogger(__name__)
 app = FastAPI()
-
-# CONFIG_TASKGRAPH = None
-
-# @app.on_event("startup")
-# def load_config():
-#     global CONFIG_TASKGRAPH
-#     parser = argparse.ArgumentParser(description="Start FastAPI with custom config.")
-#     parser.add_argument("--config_taskgraph", type=str, required=True, help="Path to the task graph configuration.")
-#     args, _ = parser.parse_known_args()  # Allows FastAPI/uvicorn to pass unknown args
-#     CONFIG_TASKGRAPH = args.config_taskgraph
-#     if not CONFIG_TASKGRAPH:
-#         raise ValueError("CONFIG_TASKGRAPH argument is required.")
-
-process = None  # Global reference for the FastAPI subprocess
-
-def terminate_subprocess():
-    """Terminate the FastAPI subprocess."""
-    global process
-    if process and process.poll() is None:  # Check if process is running
-        logger.info(f"Terminating FastAPI process with PID: {process.pid}")
-        process.terminate()  # Send SIGTERM
-        process.wait()  # Ensure it stops
-        logger.info("FastAPI process terminated.")
-
-# Register cleanup function to run on program exit
-atexit.register(terminate_subprocess)
-
-# Handle signals (e.g., Ctrl+C)
-signal.signal(signal.SIGINT, lambda signum, frame: exit(0))
-signal.signal(signal.SIGTERM, lambda signum, frame: exit(0))
 
 
 def get_api_bot_response(args, history, user_text, parameters, env):
@@ -62,29 +24,6 @@ def get_api_bot_response(args, history, user_text, parameters, env):
     result = orchestrator.get_response(data)
 
     return result['answer'], result['parameters']
-
-
-def start_apis():
-    """Start the FastAPI subprocess and update task graph API URLs."""
-    global process
-    
-    command = [
-        "uvicorn",
-        "arklex.orchestrator.NLU.api:app",  # Replace with proper import path
-        "--port", API_PORT,
-        "--host", "0.0.0.0",
-        "--log-level", "info"
-    ]
-
-    # Redirect FastAPI logs to a file
-    with open("./logs/model_api.log", "w") as log_file:
-        process = subprocess.Popen(
-            command,
-            stdout=log_file,  # Redirect stdout to a log file
-            stderr=subprocess.STDOUT,  # Redirect stderr to the same file
-            start_new_session=True  # Run in a separate process group
-        )
-    logger.info(f"Started FastAPI process with PID: {process.pid}")
 
 
 @app.post("/eval/chat")
@@ -98,7 +37,7 @@ def predict(data: Dict):
     env = Env(
         tools = tools,
         workers = workers,
-        slotsfillapi = SLOTFILLAPI_ADDR
+        slotsfillapi = ""
     )
     answer, params = get_api_bot_response(args, history[:-1], user_text, params, env)
     return {"answer": answer, "parameters": params}
@@ -113,8 +52,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     os.environ["DATA_DIR"] = args.input_dir
     MODEL["model_type_or_path"] = args.model
-
-    start_apis()
 
     #run server
     uvicorn.run(app, host="0.0.0.0", port=args.port)
--- run.py
+++ run.py
@@ -3,47 +3,21 @@ import json
 import argparse
 import time
 import logging
-import subprocess
-import signal
-import atexit
 from dotenv import load_dotenv
 from pprint import pprint
 
-import shopify
-
 from arklex.utils.utils import init_logger
 from arklex.orchestrator.orchestrator import AgentOrg
-# from create import API_PORT
 from arklex.utils.model_config import MODEL
 from arklex.utils.model_provider_config import LLM_PROVIDERS
 from arklex.env.env import Env
 
 load_dotenv()
-# session = shopify.Session(os.environ["SHOPIFY_SHOP_URL"], os.environ["SHOPIFY_API_VERSION"], os.environ["SHOPIFY_ACCESS_TOKEN"])
-# shopify.ShopifyResource.activate_session(session)
-
-process = None  # Global reference for the FastAPI subprocess
 
 def pprint_with_color(data, color_code="\033[34m"):  # Default to blue
     print(color_code, end="")  # Set the color
     pprint(data)
     print("\033[0m", end="")  
-
-def terminate_subprocess():
-    """Terminate the FastAPI subprocess."""
-    global process
-    if process and process.poll() is None:  # Check if process is running
-        logger.info(f"Terminating FastAPI process with PID: {process.pid}")
-        process.terminate()  # Send SIGTERM
-        process.wait()  # Ensure it stops
-        logger.info("FastAPI process terminated.")
-
-# Register cleanup function to run on program exit
-atexit.register(terminate_subprocess)
-
-# Handle signals (e.g., Ctrl+C)
-signal.signal(signal.SIGINT, lambda signum, frame: exit(0))
-signal.signal(signal.SIGTERM, lambda signum, frame: exit(0))
 
 
 def get_api_bot_response(args, history, user_text, parameters, env):
@@ -52,29 +26,6 @@ def get_api_bot_response(args, history, user_text, parameters, env):
     result = orchestrator.get_response(data)
 
     return result['answer'], result['parameters']
-
-
-def start_apis():
-    """Start the FastAPI subprocess and update task graph API URLs."""
-    global process
-    command = [
-        "uvicorn",
-        "arklex.orchestrator.NLU.api:app",  # Replace with proper import path
-        "--port", API_PORT,
-        "--host", "0.0.0.0",
-        "--log-level", "info"
-    ]
-
-    # Redirect FastAPI logs to a file
-    with open("./logs/api.log", "w") as log_file:
-        process = subprocess.Popen(
-            command,
-            stdout=log_file,  # Redirect stdout to a log file
-            stderr=subprocess.STDOUT,  # Redirect stderr to the same file
-            start_new_session=True  # Run in a separate process group
-        )
-    logger.info(f"Started FastAPI process with PID: {process.pid}")
-
 
 
 if __name__ == "__main__":
@@ -89,9 +40,6 @@ if __name__ == "__main__":
     MODEL["llm_provider"] = args.llm_provider
     log_level = getattr(logging, args.log_level.upper(), logging.WARNING)
     logger = init_logger(log_level=log_level, filename=os.path.join(os.path.dirname(__file__), "logs", "arklex.log"))
-
-    # Initialize NLU and Slotfill APIs
-    # start_apis()
 
     # Initialize env
     config = json.load(open(os.path.join(args.input_dir, "taskgraph.json")))
@@ -111,16 +59,15 @@ if __name__ == "__main__":
             break
     history.append({"role": worker_prefix, "content": start_message})
     pprint_with_color(f"Bot: {start_message}")
-    try:
-        while True:
-            user_text = input("You: ")
-            if user_text.lower() == "quit":
-                break
-            start_time = time.time()
-            output, params = get_api_bot_response(args, history, user_text, params, env)
-            history.append({"role": user_prefix, "content": user_text})
-            history.append({"role": worker_prefix, "content": output})
-            print(f"getAPIBotResponse Time: {time.time() - start_time}")
-            pprint_with_color(f"Bot: {output}")
-    finally:
-        terminate_subprocess()  # Ensure the subprocess is terminated
+
+    while True:
+        user_text = input("You: ")
+        if user_text.lower() == "quit":
+            break
+        start_time = time.time()
+        output, params = get_api_bot_response(args, history, user_text, params, env)
+        history.append({"role": user_prefix, "content": user_text})
+        history.append({"role": worker_prefix, "content": output})
+        print(json.dumps(params, indent=4))
+        print(f"getAPIBotResponse Time: {time.time() - start_time}")
+        pprint_with_color(f"Bot: {output}")
