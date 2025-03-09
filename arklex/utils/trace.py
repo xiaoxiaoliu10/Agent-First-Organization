21456740a5d4be30d9682048561ae953e1be2e10
Yunan Lu <luyunan0404@gmail.com>
2025-03-08 23:45:38 -0500

    convert slot into json serializable in metadata trace

--- arklex/utils/trace.py
+++ arklex/utils/trace.py
@@ -15,7 +15,10 @@ def format_meta(response_state, params, node_info=None):
         tool_input = None
     else:
         tool_info = node_info
-        tool_input = response_state.get("slots", {})
+        tool_input = {}
+        for tool_name, slots in tool_input.items():
+            tool_input[tool_name] = [s.model_dump() for s in slots]
+
         
     tool_output = response_state.get("response", "") or response_state.get("message_flow", "")
     tool_response = copy.deepcopy(response_state.get("metadata", {}).get("tool_response", {}))
